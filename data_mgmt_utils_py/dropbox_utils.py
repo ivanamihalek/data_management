38beded144d5b87807b899df1af34d5ad757093b
Ivana Mihalek <ivana.mihalek@gmail.com>
2016-07-30 15:53:40 -0400

    request error

--- data_mgmt_utils_py/dropbox_utils.py
+++ data_mgmt_utils_py/dropbox_utils.py
@@ -74,9 +74,16 @@ def upload(dbx, local_file_path, dbx_path):
                     dbx.files_upload_session_append(f.read(CHUNK_SIZE), cursor.session_id, cursor.offset)
                 except dropbox.exceptions.ApiError as err:
                     print "Chunk upload failure:", err
+                except requests.exceptions.ConnectionErroras as err:
+                    # if I get  error(104, 'Connection reset by peer'), I should probably go back to repoening the connection
+                    print "Connection error", err
+                    exit(1)
+                except :
+                     print("Unexpected error:", sys.exc_info()[0])
+                finally :
                     panic_ctr += 1
                     if panic_ctr > MAX_RETRIES:
-                        print "REached max number of retries. Bailing out."
+                        print "Reached max number of retries. Bailing out."
                         exit(1)
                     print "Will retry ..."
                     continue
