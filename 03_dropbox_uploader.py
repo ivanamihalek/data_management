e59bc820b9842f54bbf9f9f557dc7b217b5521e7
Ivana Mihalek <ivana.mihalek@gmail.com>
2016-07-30 16:02:32 -0400

    request error

--- 03_dropbox_uploader.py
+++ 03_dropbox_uploader.py
@@ -30,7 +30,7 @@ def upload_with_existence_checking(dbx, dropbox_folder, local_dir, subfolder, fi
 ####################################
 def main():
 
-    local_dir      = "/data01"
+    local_dir      = "/data02"
     dropbox_folder = "/raw_data"
 
     if not check_local_path(local_dir): exit(1)
