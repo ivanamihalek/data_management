934d39e8c759d5b3dfb50e963f2469759cf44545
Ivana Mihalek <ivana.mihalek@gmail.com>
2016-08-01 09:56:55 -0400

    full upload

--- 03_dropbox_uploader.py
+++ 03_dropbox_uploader.py
@@ -42,6 +42,7 @@ def main():
         subfolder = dirpath[len(local_dir):].strip(os.path.sep)
         for file in files:
             #if not file[-3:] in ["bz2", ".gz"]: continue
+            if file[-3:] == "bam": continue
             print subfolder, file
             upload_with_existence_checking(dbx, dropbox_folder, local_dir, subfolder, file)
 
