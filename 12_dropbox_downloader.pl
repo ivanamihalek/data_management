52560eda858e34018b6a72a77220064a56bf1f15
Ivana Mihalek <ivana.mihalek@gmail.com>
2016-09-29 10:19:05 -0400

    dropbox download

--- 12_dropbox_downloader.pl
+++ 12_dropbox_downloader.pl
@@ -29,6 +29,7 @@ def download_with_existence_checking(dbx, dbx_path, local_file_path):
         dirpath = os.path.dirname(local_file_path)
         if not os.path.exists(dirpath): os.makedirs(dirpath)
         try:
+            #tryTypeError: 'FileMetadata' object is not iterable
             dbx.files_download_to_file (local_file_path, dbx_path)
         except dropbox.files.DownloadError as err:
             print('*** download  error', err)
@@ -47,7 +48,7 @@ def scan_through_folder (dbx, dropbox_folder, subfolder, local_dir):
         exit(1)
     else:
         for entry in response.entries:
-            if type(entry)==dropbox.files.FileMetadata: continue
+            if type(entry)!=dropbox.files.FileMetadata: continue
             print entry.name
             print entry.path_display
             dbx_path = entry.path_display
