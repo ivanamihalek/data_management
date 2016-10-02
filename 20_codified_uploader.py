37f9c3bb65929c08f9ffedcadd2f33d598312823
Ivana Mihalek <ivana.mihalek@gmail.com>
2016-10-02 12:48:54 -0400

    codified

--- 20_codified_uploader.py
+++ 20_codified_uploader.py
@@ -18,7 +18,7 @@ def get_family_info (case_boid):
     cursor = db.cursor()
     switch_to_db (cursor, 'blimps_production')
     qry = 'select i.boid, i.gender, i.relationship, c.affected  from individuals as i, clinical_data as c'
-    qry += 'where i.boid like "%s%% and i.id=c.individual_id" ' % case_boid 
+    qry += 'where i.boid like "%s%%" and i.id=c.individual_id ' % case_boid 
     rows  = search_db (cursor, qry)
     for row in rows:
         print row
