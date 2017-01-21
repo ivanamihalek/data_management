8957f852482705d61eefb8cfe7bd2517176be4db
ivana <ivana.mihalek@gmail.com>
2017-01-21 16:52:47 -0500

    cram

--- 01_cram2bam.pl
+++ 01_cram2bam.pl
@@ -13,12 +13,23 @@ my $samtools = "/usr/local/bin/samtools";
 # the header conaints @SQ linies with UR fields, all UR fields are the same, nad contain the path to
 # a human assembly that I already have stored
 my @lines = split "\n", `$samtools view -H $cramfile`;
+my @assemblies;
 for my $line (@lines) {
     $line =~  /\sUR\:(\S+)(\s|$)/|| next;
     my @aux = split "\/", $1;
     my $assembly = pop(@aux);
-    print $assembly , "\n";
+    my @foo = grep($assembly, @assemblies);
+    next if @foo>0;
+    @assemblies.push($assembly);
 }
+
+@assemblies==0 && die "no candidate assembly found in the header of $cramfile\n";
+@assemblies>1 && die "I am not equipped to handle more than one assembly: @assemnlies\n";
+
+my $reference_assembly = @assemblies[0];
+
+print " reference assembly: $reference_assembly  \n";
+# do we have the assembly at the expected location?
 
 # we have found the assembly, is it indexed?
 
