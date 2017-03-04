f5449f060bf20ee4f2f0c93622096eeefb4f4beb
ivana <ivana.mihalek@gmail.com>
2017-03-04 15:18:12 -0500

    fixing the archived mess

--- hack.py
+++ hack.py
@@ -11,7 +11,8 @@ def main():
 	for dirpath, dirs, files in os.walk(local_dir):
 		if not 'archived' in dirs: continue
 		print dirpath, 'archived'
-		os.system("ls %s/archived > %s/RCHIVED " % (dirpath, dirpath))
+		os.system("rm -rf %s/archived/archived  " % dirpath)
+		os.system("ls %s/archived > %s/ARCHIVED " % (dirpath, dirpath))
 		exit()
 
 ####################################
