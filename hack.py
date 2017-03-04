69c54962b07eb4f32110ac295bd3f643769a74f2
ivana <ivana.mihalek@gmail.com>
2017-03-04 15:23:55 -0500

    fixing the archived mess

--- hack.py
+++ hack.py
@@ -14,7 +14,6 @@ def main():
 		os.system("rm -rf %s/archived/archived  " % dirpath)
 		os.system("ls %s/archived > %s/ARCHIVED " % (dirpath, dirpath))
 		os.system("rm -rf %s/archived " % dirpath)
-		exit()
 
 ####################################
 if __name__ == '__main__':
