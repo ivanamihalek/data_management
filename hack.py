846ef7187b0fb8015f98bd7c2e1c37c449d78793
ivana <ivana.mihalek@gmail.com>
2017-03-04 15:01:31 -0500

    fixing the archived mess

--- hack.py
+++ hack.py
@@ -6,12 +6,12 @@ import os
 ####################################
 def main():
 
-	local_dir      = "/data01"
+	local_dir = "/data01"
 
 	for dirpath, dirs, files in os.walk(local_dir + "/" + subdir):
 		subfolder = dirpath[len(local_dir):].strip(os.path.sep)
 		if not 'archived' in dirs: continue
-		print dirpath, 'archived'
+		print dirpath, 'archived', subfolder
 
 ####################################
 if __name__ == '__main__':
