fbec100755a2fc6034744ea2910fa711c742a1f7
ivana <ivana.mihalek@gmail.com>
2017-03-04 15:23:12 -0500

    fixing the archived mess

--- hack.py
+++ hack.py
@@ -13,6 +13,7 @@ def main():
 		print dirpath, 'archived'
 		os.system("rm -rf %s/archived/archived  " % dirpath)
 		os.system("ls %s/archived > %s/ARCHIVED " % (dirpath, dirpath))
+		os.system("rm -rf %s/archived " % dirpath)
 		exit()
 
 ####################################
