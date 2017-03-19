a3f10ac2ef5fd1b72d8e2ec712a0a0367f2d5fed
ivana <ivana.mihalek@gmail.com>
2017-03-19 19:45:50 -0400

    upload without existence check

---
+++ 11_dropbox_upload_wo_existence_checking.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+
+
+from  data_mgmt_utils_py.generic_utils import *
+from  data_mgmt_utils_py.dropbox_utils import *
+
+
+####################################
+DROPBOX_TOKEN = os.environ['DROPBOX_TOKEN']
+dbx = dropbox.Dropbox(DROPBOX_TOKEN)
+
+
+####################################
+def upload_no_existence_checking(dbx, dropbox_folder, local_dir, subfolder, file_to_move):
+
+    subdir = subfolder
+    local_file_path = "/".join([local_dir, subdir, file_to_move])
+    dbx_path =  "/".join([dropbox_folder, subfolder, file_to_move])
+    print dbx_path + "uploading without existence check"
+    exit()
+    upload (dbx, local_file_path, dbx_path)
+
+####################################
+def main():
+
+    local_dir      = "/data01"
+    dropbox_folder = "/raw_data"
+
+    if not check_local_path(local_dir): exit(1)
+    if not check_dbx_path (dbx, dropbox_folder): exit(1)
+
+    subdir = "2012/002"
+
+    for dirpath, dirs, files in os.walk(local_dir + "/" + subdir):
+        subfolder = dirpath[len(local_dir):].strip(os.path.sep)
+        for file in files:
+            if file[-10:] != "bedcov.csv": continue
+            if file=='ARCHIVED': continue
+            print subfolder, file
+            upload_no_existence_checking(dbx, dropbox_folder, local_dir, subfolder, file)
+
+
+####################################
+if __name__ == '__main__':
+    main()
