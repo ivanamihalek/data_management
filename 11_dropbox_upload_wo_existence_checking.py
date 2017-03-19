1ada10a2cbb1317d8937af0f651a6895f9a5c30b
ivana <ivana.mihalek@gmail.com>
2017-03-19 19:47:14 -0400

    upload without existence check

--- 11_dropbox_upload_wo_existence_checking.py
+++ 11_dropbox_upload_wo_existence_checking.py
@@ -29,7 +29,7 @@ def main():
     if not check_local_path(local_dir): exit(1)
     if not check_dbx_path (dbx, dropbox_folder): exit(1)
 
-    subdir = "2012/002"
+    subdir = "2016/022"
 
     for dirpath, dirs, files in os.walk(local_dir + "/" + subdir):
         subfolder = dirpath[len(local_dir):].strip(os.path.sep)
