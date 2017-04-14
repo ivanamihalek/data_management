56b1b6666b269e5944262286a41e85e1bf164462
Ivana Mihalek <ivana.mihalek@gmail.com>
2017-04-14 14:54:46 -0400

    housekeeping

--- 04_md5sum_checker.pl
+++ 04_md5sum_checker.pl
@@ -12,13 +12,13 @@ foreach my $md5_file_path (@md5_files)  {
     my @aux = split "/", $md5_file_path;
     my $md5_file = pop @aux;
     my $file = $md5_file; $file =~ s/\.md5$//;
-    print "$md5_file\n";
+    print "\n";
     print "$file\n";
     my $ret =  `grep $file $source_md5sum_file`;
     @aux = split /\s+/, $ret;
     my $source_md5 = $aux[0];
     my $target_md5 = `cat $md5_file_path`; chomp $target_md5;
-    print "$source_md5\n $target_md5\n";
+    print "src: $source_md5\ntgt: $target_md5\n";
 
 }
 
