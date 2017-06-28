0ed91afe406e81f7e632c35375c82d118acc7431
Ivana Mihalek <ivana.mihalek@gmail.com>
2017-06-28 17:28:59 -0400

    rotkvica

---
+++ 15_dropbox_meta_file.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python
+
+import os
+
+from  data_mgmt_utils_py.generic_utils import *
+from  data_mgmt_utils_py.dropbox_utils import *
+
+
+####################################
+DROPBOX_TOKEN = os.environ['DROPBOX_TOKEN']
+
+dbx = dropbox.Dropbox(DROPBOX_TOKEN)
+
+def extension_checks(filename):
+    for ext in ["bam", "fastq", "fastq.bz2", "fastq.gz"]:
+        if len(filename)>len(ext) and filename[-len(ext):]==ext: return ext
+    return False
+
+####################################
+def scan_through_folder (dbx, dropbox_folder, subfolder):
+    boids = []
+    fastqs = {}
+    bams   = {}
+    dbx_path = "/".join([dropbox_folder, subfolder])
+    print dbx_path
+    try:
+        response = dbx.files_list_folder(dbx_path, recursive = True)
+    except dropbox.exceptions.ApiError as err:
+        print('Folder listing failed for', dbx_path, '-- assumed empty:', err)
+        exit(1)
+    else:
+        #print " ** ", response
+        for entry in response.entries:
+            if type(entry)!=dropbox.files.FileMetadata: continue
+            if not "reads" in entry.path_display and not "alignments/by_seq_center" in entry.path_display: continue
+            ext = extension_checks(entry.name)
+            if not ext: continue
+            field =  entry.path_display.split("/")
+            boid = field[4]
+            file = field[-1]
+            if not boid in boids: boids.append(boid)
+            if "bam" in ext:
+                if not  bams.has_key(boid): bams[boid] = []
+                bams[boid].append(entry.path_display)
+            elif "fastq" in ext:
+                if not  fastqs.has_key(boid): fastqs[boid] = []
+                fastqs[boid].append(entry.path_display)
+            #dbx_path = entry.path_display
+            #local_path = dbx_path.replace(dropbox_folder,local_dir)
+    return boids, fastqs, bams
+
+####################################
+def main():
+
+    dropbox_folder = "/raw_data"
+
+    if not check_dbx_path (dbx, dropbox_folder): exit(1)
+
+    #for subfolder in ["2011","2012","2013","2014","2015","2016","2017"]:
+    for subfolder in ["2011"]:
+        print " *************** ", subfolder, " ****************** "
+        boids, fastqs, bams = scan_through_folder(dbx, dropbox_folder, subfolder)
+        for boid in sorted(boids):
+            if not fastqs.has_key(boid) and not bams.has_key(): continue
+            out_list = []
+            out_list.append(boid)
+            if fastqs.has_key(boid): out_list += fastqs[boid]
+            if bams.has_key(boid): out_list += bams[boid]
+
+            print "\t".join(out_list)
+
+####################################
+if __name__ == '__main__':
+    main()
