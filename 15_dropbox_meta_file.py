4e371f6bffadf783fa3129f76464973aa366e543
Ivana Mihalek <ivana.mihalek@gmail.com>
2017-06-28 17:46:42 -0400

    rotkvica

--- 15_dropbox_meta_file.py
+++ 15_dropbox_meta_file.py
@@ -74,6 +74,7 @@ def main():
 
             qry = 'select i.boid, i.gender, i.relationship, c.affected, x.xref from individuals as i, clinical_data as c, identifiers as x'
             qry += 'where i.boid="%s" and i.id="%s" and x.name="%s" ' % (boid, boid, boid)
+            print qry
             rows  = search_db (cursor, qry)
             for row in rows:
                 [boid, gender, relationship, affected, xrefs]  = row
